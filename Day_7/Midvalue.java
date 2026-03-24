3139d0a84c5591461b7ba684be67c23c7f1bd7cd
POOVARASAN S <163829919+S-poovarasan@users.noreply.github.com>
2026-03-24 12:09:08 +0530

    Add files via upload

---
+++ Day_7/Midvalue.java
@@ -0,0 +1,52 @@
+//middle in linkedlist
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int value)
+    {
+        this.data=value;
+        this.next=null;
+    }
+}
+class Solution
+{
+    Node middleNode(Node head)
+    {
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
+public class Midvalue
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Node head=null,tail=null;
+        for(int i=0;i<n;i++)
+        {
+            int val=sc.nextInt();
+            Node newNode=new Node(val);
+            if(head==null)
+            {
+                head=tail=newNode;
+            }
+            else
+            {
+                tail.next=newNode;
+                tail=newNode;
+            }
+        }
+        Solution obj=new Solution();
+        head=obj.middleNode(head);
+        System.out.println(head.data);
+    }
+
+}
